224c95f1c7a452a95b6dfc0a5b65402126221615
varun <varun.patel@mindinventory.com>
2025-12-25 12:26:04 +0530

    some change

--- database.js
+++ database.js
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 import { EnvConfig } from "./config/EnvConfig.js";
 
 export default function connectToDatabase() {
-    const db_url = EnvConfig.MONGO_DB_CONNECTION_URL
+  const db_url = EnvConfig.MONGO_DB_CONNECTION_URL;
 
-    try {
-        
-        mongoose.connect(db_url);
-        console.log("Connected To The Db Successfully");
-    } catch (error) {
-        console.error(error)
-        process.exit(1);
-    }
-
+  try {
+    mongoose.connect(db_url, {
+      serverSelectionTimeoutMS: 5000,
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+      retryWrites: true,
+    });
+    console.log("Connected To The Db Successfully");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
